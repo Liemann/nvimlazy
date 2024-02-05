0ea4cd20ca3b621a6ec8541da42797008898aa89
Li3man <max.liman@student.chalmer.se>
2024-02-05 09:45:19 +0100

    Added close buffer keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,9 +24,10 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
-keymap("n", "<leader>q", ":bw<CR>", opts)
+keymap("n", "<leader>q", ":q<CR>", opts)
 keymap("n", "<leader>s", "<C-w>s", opts)
 keymap("n", "<leader>v", "<C-w>v", opts)
+keymap("n", "<leader>w", ":bp|bd #<CR>", opts)
 
 --keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
